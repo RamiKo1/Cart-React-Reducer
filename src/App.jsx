ccb3e8ba426b113125b3deb82fbbfa67e691f889
Rami_Kot <ramikr63@gmail.com>
2023-09-28 19:11:11 +0300

    final

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// components
+import Navbar from './Navbar'
+import CartContainer from './CartContainer'
+import { useGlobalContext } from './Context'
+
+function App() {
+  const { loading } = useGlobalContext()
+  if (loading) {
+    return <div className="loading" style={{ marginTop: '6rem' }}></div>
+  }
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  )
+}
+
+export default App
